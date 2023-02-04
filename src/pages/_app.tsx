ad490fd5aca7b5c9cdc5ff9137e9af99aa41eef3
Brice Suazo <bricesuazo@gmail.com>
2023-02-05 00:23:00 +0800

    initialize app.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
+
+import { api } from "../utils/api";
+
+import "../styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <>
+      <Head>
+        <title>LogPic</title>
+        <meta name="description" content="LogPic" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
+  );
+};
+
+export default api.withTRPC(MyApp);
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next";
+
+import { api } from "../utils/api";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <main>main</main>
+    </>
+  );
+};
+
+export default Home;
