449962da26743619ccc5f455c0d15c020bcdb93e
Brice Suazo <bricesuazo@gmail.com>
2023-02-06 09:14:46 +0800

    Add logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -6,12 +7,15 @@ const Header = () => {
   const session = useSession();
   return (
     <header className="flex items-center justify-between bg-green-800 p-4 text-white">
-      <Link href="/">
-        <h1>LogPic</h1>
+      <Link href="/" className="relative h-12 w-12">
+        <Image src="/logo.png" alt="" fill />
       </Link>
       {session.status === "authenticated" && (
         <div className="flex items-center gap-x-2">
-          <p>{session.data.user.id}</p>
+          <p>
+            {session.data.user.id} - {session.data.user.first_name}{" "}
+            {session.data.user.last_name}
+          </p>
           <button
             className="rounded bg-green-600 p-2"
             onClick={() =>
