40a7de88de00bddf29f84095936e488f2fb4ca6a
Brice Suazo <bricesuazo@gmail.com>
2023-02-06 14:26:21 +0800

    Add styling to HR and employee login.

--- src/pages/employee-login.tsx
+++ src/pages/employee-login.tsx
@@ -13,50 +13,71 @@ const EmployeeLogin = () => {
   });
 
   return (
-    <div>
-      <h1>Employee Login</h1>
+    <main className="mx-auto max-w-screen-lg py-56 px-4 md:flex">
+      <div className="flex flex-1 flex-col justify-center gap-8 p-8">
+        <h1 className="text-center text-4xl font-bold">Employee</h1>
 
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-
-          void (async () => {
-            await signIn("credentials", {
-              type: "EMPLOYEE",
-              id: employeeLogin.uniqueNumber,
-              password: employeeLogin.password,
-              callbackUrl: "/employee-dashboard",
-            });
-          })();
-        }}
-      >
-        <label htmlFor="unique-number">Unique Number</label>
-        <input
-          type="text"
-          id="unique-number"
-          required
-          onChange={(e) => {
-            setEmployeeLogin({
-              ...employeeLogin,
-              uniqueNumber: e.target.value,
-            });
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            void (async () => {
+              await signIn("credentials", {
+                type: "EMPLOYEE",
+                id: employeeLogin.uniqueNumber,
+                password: employeeLogin.password,
+                callbackUrl: "/employee-dashboard",
+              });
+            })();
           }}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          required
-          onChange={(e) => {
-            setEmployeeLogin({
-              ...employeeLogin,
-              password: e.target.value,
-            });
-          }}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+          className="flex flex-col items-center justify-center gap-y-4"
+        >
+          <div className="flex gap-x-4">
+            <div className="flex flex-col">
+              <label htmlFor="unique-number" className="text-sm">
+                Unique Number <span className="text-red-500">*</span>
+              </label>
+              <input
+                placeholder="Enter your unique number"
+                type="number"
+                id="unique-number"
+                required
+                onChange={(e) => {
+                  setEmployeeLogin({
+                    ...employeeLogin,
+                    uniqueNumber: e.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="password" className="text-sm">
+                Password <span className="text-red-500">*</span>
+              </label>
+              <input
+                placeholder="Enter your password"
+                type="password"
+                id="password"
+                required
+                onChange={(e) => {
+                  setEmployeeLogin({
+                    ...employeeLogin,
+                    password: e.target.value,
+                  });
+                }}
+              />
+            </div>
+          </div>
+          <button type="submit" className="button w-32 !rounded-full">
+            Sign in
+          </button>
+        </form>
+      </div>
+      <div className="flex-1 space-y-4 p-8">
+        <h1 className="text-8xl font-semibold text-black">Log-in</h1>
+        <h1 className="text-8xl font-semibold text-[#206e32]">Upload</h1>
+        <h1 className="text-8xl font-semibold text-[#fc0505]">Track</h1>
+      </div>
+    </main>
   );
 };
 
--- src/pages/hr-login.tsx
+++ src/pages/hr-login.tsx
@@ -13,50 +13,72 @@ const HRLogin = () => {
   });
 
   return (
-    <div>
-      <h1>HR Login</h1>
+    <main className="mx-auto max-w-screen-lg py-56 px-4 md:flex">
+      <div className="flex flex-1 flex-col justify-center gap-8 p-8">
+        <h1 className="text-center text-4xl font-bold">Human Resources</h1>
 
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
 
-          void (async () => {
-            await signIn("credentials", {
-              type: "HR",
-              id: employeeLogin.email,
-              password: employeeLogin.password,
-              callbackUrl: "/hr-dashboard",
-            });
-          })();
-        }}
-      >
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          required
-          onChange={(e) => {
-            setEmployeeLogin({
-              ...employeeLogin,
-              email: e.target.value,
-            });
+            void (async () => {
+              await signIn("credentials", {
+                type: "HR",
+                id: employeeLogin.email,
+                password: employeeLogin.password,
+                callbackUrl: "/hr-dashboard",
+              });
+            })();
           }}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          required
-          onChange={(e) => {
-            setEmployeeLogin({
-              ...employeeLogin,
-              password: e.target.value,
-            });
-          }}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+          className="flex flex-col items-center justify-center gap-y-4"
+        >
+          <div className="flex gap-x-4">
+            <div className="flex flex-col">
+              <label htmlFor="email" className="text-sm">
+                Email <span className="text-red-500">*</span>
+              </label>
+              <input
+                placeholder="Enter your email"
+                type="email"
+                id="email"
+                required
+                onChange={(e) => {
+                  setEmployeeLogin({
+                    ...employeeLogin,
+                    email: e.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="password" className="text-sm">
+                Password <span className="text-red-500">*</span>
+              </label>
+              <input
+                placeholder="Enter your password"
+                type="password"
+                id="password"
+                required
+                onChange={(e) => {
+                  setEmployeeLogin({
+                    ...employeeLogin,
+                    password: e.target.value,
+                  });
+                }}
+              />
+            </div>
+          </div>
+          <button type="submit" className="button w-32 !rounded-full">
+            Sign in
+          </button>
+        </form>
+      </div>
+      <div className="flex-1 space-y-4 p-8">
+        <h1 className="text-8xl font-semibold text-black">Log-in</h1>
+        <h1 className="text-8xl font-semibold text-[#206e32]">Upload</h1>
+        <h1 className="text-8xl font-semibold text-[#fc0505]">Track</h1>
+      </div>
+    </main>
   );
 };
 
